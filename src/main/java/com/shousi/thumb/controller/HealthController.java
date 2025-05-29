e74d1e2e496ba3ab7a7f090b8e6bb92b0e3d228a
shousi18 <1416239139@qq.com>
2025-05-29 22:35:40 +0800

    feat:配置knife4j;进行健康接口检测;

---
+++ src/main/java/com/shousi/thumb/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.shousi.thumb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthController {
+    @GetMapping("/check")
+    public String check() {
+        return "ok";
+    }
+}
